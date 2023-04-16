54073cbaefdb2e64779a7a22dba9b145dd11b450
ZevLipsky <zevilipsky@gmail.com>
2023-04-16 16:53:14 +0300

    profile

---
+++ my-portfolio/client/src/PortfolioContainer/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Profile from './Profile'
+import Footer from './Footer/Footer'
+
+export default function Home() {
+  return (
+    <div className='home-container'>
+        <Profile />
+        <Footer />
+
+    </div>
+  )
+}

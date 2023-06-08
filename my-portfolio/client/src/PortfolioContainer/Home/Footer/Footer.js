25eb480351646edf4e3fd5904fff349ff7a70aef
ZevLipsky <zevilipsky@gmail.com>
2023-06-08 15:28:22 +0300

    added scroll to hire

--- my-portfolio/client/src/PortfolioContainer/Home/Footer/Footer.js
+++ my-portfolio/client/src/PortfolioContainer/Home/Footer/Footer.js
@@ -5,10 +5,10 @@ export default function footer() {
   return (
     <div className="footer-container">
       <div className="footer-parent">
-        <img
-          // src={require("../../../assets/Home/shape-bg.png").default}
+        {/* <img
+          src={require("../../../assets/Home/shape-bg.png").default}
           alt="you have problem with the image"
-        />
+        /> */}
       </div>
     </div>
   );
--- my-portfolio/client/src/PortfolioContainer/Home/Profile/Profile.js
+++ my-portfolio/client/src/PortfolioContainer/Home/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Typical from 'react-typical';
 import './Profile.css';
+import ScrollService from '../../../utilities/ScrollService';
 
 export default function Profile() {
   return (
@@ -59,7 +60,9 @@ export default function Profile() {
                 </span>
             </div>
             <div className='profile-options'>
-                <button className='btn primary-btn'>
+                <button className='btn primary-btn'
+                onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+                >
                     {""}
                     Hire Me{" "}
                 </button>
